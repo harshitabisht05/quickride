d8879d7112cb286fa0c04b6604782fa7533dc69a
harshitabisht05 <harshitabisht0515@gmail.com>
2026-06-24 22:34:44 +0530

    Add analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+    <Analytics/>
   </BrowserRouter>
 );
